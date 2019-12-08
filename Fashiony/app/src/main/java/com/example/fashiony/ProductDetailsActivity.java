e8d39bda948ca1722aa02eb878d345b0842ffef9
aida <america125>
2019-12-08 21:05:28 +0600

    fix cart list

--- Fashiony/app/src/main/java/com/example/fashiony/ProductDetailsActivity.java
+++ Fashiony/app/src/main/java/com/example/fashiony/ProductDetailsActivity.java
@@ -71,7 +71,7 @@ public class ProductDetailsActivity extends AppCompatActivity {
         SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
         saveCurrentTime = currentTime.format(calForDate.getTime());
 
-        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference();
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
 
         final HashMap<String, Object> cartMap = new HashMap<>();
         cartMap.put("pid", productID);
